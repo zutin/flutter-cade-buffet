101c3a92eb6e0812b93e6a2b706545309ee97276
zutin <gianluccaz@hotmail.com>
2024-05-24 17:13:51 -0300

    feat: Added fetch to Buffets API

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MainApp());
@@ -15,12 +18,101 @@ class MainApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+// MODELS CLASSES
+
+Future<List<Buffet>> fetchBuffets() async {
+  final response = await http.get(Uri.parse('http://localhost:3000/api/v1/buffets'));
+  if (response.statusCode == 200) {
+    List<dynamic> buffetsJson = jsonDecode(response.body) as List<dynamic>;
+    return buffetsJson.map((json) => Buffet.fromJson(json as Map<String, dynamic>)).toList();
+  } else {
+    throw Exception('Failed to load buffets.');
+  }
+}
+
+class Buffet {
+  final String trading_name;
+  final String contact_number;
+  final String email;
+  final String address;
+  final String district;
+  final String state;
+  final String city;
+  final String zipcode;
+  final String description;
+  final String payment_methods;
+  final double average_rating;
+
+  const Buffet({
+    required this.trading_name,
+    required this.contact_number,
+    required this.email,
+    required this.address,
+    required this.district,
+    required this.state,
+    required this.city,
+    required this.zipcode,
+    required this.description,
+    required this.payment_methods,
+    required this.average_rating,
+  });
+
+  factory Buffet.fromJson(Map<String, dynamic> json) {
+    return Buffet(
+      trading_name: json['trading_name'] as String,
+      contact_number: json['contact_number'] as String,
+      email: json['email'] as String,
+      address: json['address'] as String,
+      district: json['district'] as String,
+      state: json['state'] as String,
+      city: json['city'] as String,
+      zipcode: json['zipcode'] as String,
+      description: json['description'] as String,
+      payment_methods: json['payment_methods'] as String,
+      average_rating: json['average_rating'] as double,
+    );
+  }
+}
+
+// PAGES CLASSES
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late Future<List<Buffet>> buffets;
+
+  @override
+  void initState() {
+    super.initState();
+    buffets = fetchBuffets();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Text('Oi');
+    return FutureBuilder<List<Buffet>>(
+      future: buffets,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return ListView.builder(
+            itemCount: snapshot.data!.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(snapshot.data![index].trading_name),
+              );
+            },
+          );
+        } else if (snapshot.hasError) {
+          return Text('${snapshot.error}');
+        }
+
+        return const CircularProgressIndicator();
+      },
+    );
   }
 }
 
@@ -42,7 +134,7 @@ class AboutPage extends StatelessWidget {
   }
 }
 
-// Navigation classes
+// NAVIGATION CLASSES
 
 class ApplicationPage extends StatefulWidget {
   const ApplicationPage({super.key});
